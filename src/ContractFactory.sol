ff39a9843530c13f660a37955bd5c37a8aa97936
Hunter <hunterbhb11@gmail.com>
2025-04-12 21:27:09 -0500

    factory v1

--- src/ContractFactory.sol
+++ src/ContractFactory.sol
@@ -8,14 +8,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 error InsufficientDEVFee();
 
 contract ContractFactory is Context, Ownable {
-    address public s_owner;
-    uint256 public s_contractCreationDevFee; // This is the fee paid to the developer for creating a user contract
+    uint256 public s_devFee; // This is the fee paid to the developer for creating a user contract
+    address public s_devAddress; // The address of the developer for payment of the contract creation dev fee
 
     event ContractCreated(address indexed newContract, address indexed creator);
 
-    constructor(uint256 _contractCreationFee, uint256 _contractCreationGasFee, uint256 _contractCreationDevFee) {
-        s_owner = msg.sender;
-        s_contractCreationDevFee = _contractCreationDevFee; //Developer fee for contract creation
+    constructor(uint256 _contractCreationDevFee) {
+        s_devFee = _contractCreationDevFee; //Developer fee for contract creation
+        s_devAddress = msg.sender; // Set the contract creator as the dev address
     }
 
     function createContract(address recipient, uint256 timeToSend, bool isPullingAllowed)
@@ -23,20 +23,21 @@ contract ContractFactory is Context, Ownable {
         payable
         returns (address newContract)
     {
-        if (msg.value < s_contractCreationDevFee) revert InsufficientDEVFee();
+        if (msg.value < s_devFee) revert InsufficientDEVFee();
 
-        newContract = address(new UserCreatedContract(msg.sender, recipient, timeToSend, isPullingAllowed));
-
+        newContract =
+            address(new UserCreatedContract(msg.sender, recipient, timeToSend, block.timestamp, isPullingAllowed));
+        (bool sent,) = s_devAddress.call{value: s_devFee}("");
         emit ContractCreated(newContract, msg.sender);
-    }
-
-    function getContractCreationDevFee() public view returns (uint256) {
-        return s_contractCreationDevFee;
     }
 
     //There shouldn't be a need for a maximum requirement in the contract creation fee as safety shouldn't be affected by the fee.
     //If fee is too high, users will not use the service. That would be it.
-    function setContractCreationDevFee(uint256 newFee) external onlyOwner {
-        s_contractCreationDevFee = newFee;
+    function setDevFee(uint256 newFee) external onlyOwner {
+        s_devFee = newFee;
+    }
+
+    function setDevAddress(address newAddress) external onlyOwner {
+        s_devAddress = newAddress;
     }
 }
